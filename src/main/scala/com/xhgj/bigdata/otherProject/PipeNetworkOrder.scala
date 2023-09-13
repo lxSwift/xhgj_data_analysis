0bd7ef5ec1e8c8660865089d70f09158a529f4a4
lxSwift <372949859@qq.com>
2023-09-13 16:49:08 +0800

    管网状态代码

---
+++ src/main/scala/com/xhgj/bigdata/otherProject/PipeNetworkOrder.scala
@@ -0,0 +1,190 @@
+package com.xhgj.bigdata.otherProject
+
+import com.xhgj.bigdata.util.{AddressAnalysis, TableName}
+import org.apache.spark.sql.SparkSession
+
+/**
+ * @Author luoxin
+ * @Date 2023/9/13 9:58
+ * @PackageName:com.xhgj.bigdata.otherProject
+ * @ClassName: PipeNetworkOrder
+ * @Description: 官网客户订单数据
+ * @Version 1.0
+ */
+object PipeNetworkOrder {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("Spark task job PipeNetworkOrder.scala")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    runRES(spark)
+    //    salman(spark)
+    //关闭SparkSession
+    spark.stop()
+  }
+
+  def runRES(spark: SparkSession): Unit = {
+
+    spark.udf.register("address_get",(str:String) =>addressget(str))
+    //读取手工表在2022年的数据
+    spark.sql(
+      s"""
+         |SELECT
+         |  c_order_time,
+         |  c_order_id,
+         |  c_platform_price,
+         |  c_title,
+         |  c_receiving_province
+         |FROM
+         |  ${TableName.DWD_WRITE_PFORDER}
+         |""".stripMargin).createOrReplaceTempView("write_pforder")
+
+    //将履约平台的2023之后的数据取出来, 同一个订单类型里面的客户订单号是唯一的,c_state代表的是正常使用的数据
+    spark.sql(
+      s"""
+         |SELECT
+         |  A.c_order_time, --下单时间
+         |  A.c_docking_order_no c_order_id, --客户订单号
+         |  cast(A.c_platform_price as decimal(19,2)) - sum(cast(COALESCE(c_return_num,0) as decimal(19,2)) * cast(COALESCE(c_xs_price,0) as decimal(19,2))) c_platform_price, --平台总价
+         |  A.c_title, --发票抬头
+         |  address_get(A.c_address) c_receiving_province --收货省区
+         |FROM
+         |  ${TableName.ODS_OMS_PFORDER} A
+         |LEFT JOIN ${TableName.ODS_OMS_PFRETURNORDER} B ON A.id = B.order_id
+         |LEFT JOIN ${TableName.ODS_OMS_PFRETURNORDERDETAIL} C ON B.id = C.return_order_id
+         |where A.c_state = '1' and A.c_docking_type ='46' and substring(A.c_order_time,1,4) > '2023' and B.c_state = '1' and C.c_state = '1'
+         |group by A.c_docking_order_no,A.c_title,A.c_order_time,A.c_address,A.c_platform_price
+         |""".stripMargin).createOrReplaceTempView("oms_pforder")
+    //合并上方两个数据
+    spark.sql(
+      s"""
+         |SELECT
+         |  *
+         |FROM
+         |  write_pforder
+         |UNION ALL
+         |SELECT
+         |  *
+         |FROM
+         |  oms_pforder
+         |""".stripMargin).createOrReplaceTempView("pforder")
+
+    //从大票项目单取值
+    spark.sql(
+      s"""
+         |SELECT
+         |big.FCUSTOMERORDERID, --客户订单号
+         |big.fbillno, --项目编码
+         |sum(bige.FPRICETAXAMOUNT) FPRICETAXAMOUNT,  --价税合计(含税)
+         |sum(bige.FAMOUNT) FAMOUNT, --金额(不含税)
+         |DS.FNAME SALENAME --销售员
+         |FROM
+         |${TableName.ODS_ERP_BIGTICKETPROJECT} big
+         |JOIN ${TableName.ODS_ERP_BigTicketProjectEntry} bige on big.fid = bige.fid
+         |LEFT JOIN ${TableName.DIM_SALEMAN} DS ON big.FSALESMAN = DS.FID
+         |WHERE big.fprojectname IN('项目名称——浙20221277-国家管网集团2022年电商平台采购项目','国家管网集团2022年电商平台采购项目')
+         |group by big.fbillno,DS.FNAME,big.FCUSTOMERORDERID
+         |""".stripMargin).createOrReplaceTempView("bigproject")
+
+    //小票组织：咸亨国际科技股份有限公司、武汉咸亨国际能源科技有限公司；单据状态：已审核；
+    // 项目编号与大票项目单信息中的项目编号相匹配的单据之汇总金额；金额应当除以代开比例，代开比例为空的按100计
+    spark.sql(
+      s"""
+         |select
+         |	DP.fnumber PRONO,
+         | CAST(SUM(case when DP.FBEHALFINVOICERATIO is not null and TRIM(DP.FBEHALFINVOICERATIO) != '' and TRIM(DP.FBEHALFINVOICERATIO) != 0 then OERE.FPRICEQTY * OERE.FPRICE/DP.FBEHALFINVOICERATIO*100
+         | else OERE.FPRICEQTY * OERE.FPRICE end) AS DECIMAL(19,2)) AS SALEAMOUNT , --不含税金额
+         | CAST(SUM(case when DP.FBEHALFINVOICERATIO is not null and TRIM(DP.FBEHALFINVOICERATIO) != '' and TRIM(DP.FBEHALFINVOICERATIO) != 0 then OERE.FPRICEQTY * OERE.FTAXPRICE/DP.FBEHALFINVOICERATIO*100
+         | else OERE.FPRICEQTY * OERE.FTAXPRICE end) AS DECIMAL(19,2)) AS SALETAXAMOUNT , --含税金额
+         | sum(OERE.FCOSTAMTSUM) FCOSTAMTSUM --成本去税总金额
+         |from
+         |	${TableName.ODS_ERP_RECEIVABLE} a
+         |join ${TableName.ODS_ERP_RECEIVABLEENTRY} OERE on a.fid=OERE.fid
+         |left join ${TableName.DIM_PROJECTBASIC} DP on OERE.FPROJECTNO=DP.fid
+         |left join ${TableName.DIM_ORGANIZATIONS} org on org.forgid=a.FSETTLEORGID
+         |where a.FDOCUMENTSTATUS='C' AND org.fname in ('咸亨国际科技股份有限公司','武汉咸亨国际能源科技有限公司')
+         |group by DP.fnumber
+         |""".stripMargin).createOrReplaceTempView("smallrece")
+
+
+
+    //大票组织：DP咸亨国际科技股份有限公司；单据状态：已审核；项目编号与大票项目单信息中的项目编号相匹配的单据之汇总金额
+    spark.sql(
+      s"""
+         |select
+         |	DP.fnumber PRONO,
+         | CAST(SUM(OERE.FPRICEQTY * OERE.FTAXPRICE) AS DECIMAL(19,2)) AS SALETAXAMOUNT --含税金额
+         |from
+         |	${TableName.ODS_ERP_RECEIVABLE} a
+         |join ${TableName.ODS_ERP_RECEIVABLEENTRY} OERE on a.fid=OERE.fid
+         |left join ${TableName.DIM_PROJECTBASIC} DP on OERE.FPROJECTNO=DP.fid
+         |left join ${TableName.DIM_ORGANIZATIONS} org on org.forgid=a.FSETTLEORGID
+         |where a.FDOCUMENTSTATUS='C' AND org.fname ='DP咸亨国际科技股份有限公司'
+         |group by DP.fnumber
+         |""".stripMargin).createOrReplaceTempView("bigrece")
+
+    //收款单和收款退款单
+    spark.sql(
+      s"""
+         |SELECT DP.FNUMBER,SUM(OERE.FRECAMOUNTFOR_E) REAMOUNT
+         |FROM ${TableName.ODS_ERP_RECEIVEBILL} OER
+         |LEFT JOIN ${TableName.ODS_ERP_RECEIVEBILLENTRY} OERE ON OER.FID = OERE.FID
+         |LEFT JOIN ${TableName.DIM_PROJECTBASIC} DP ON OERE.FPROJECTNO = DP.FID
+         |left join ${TableName.DIM_ORGANIZATIONS} org on org.forgid=OER.FPAYORGID
+         |WHERE OER.FDOCUMENTSTATUS='C' AND org.fname ='DP咸亨国际科技股份有限公司'
+         |GROUP BY DP.FNUMBER
+         |""".stripMargin).createOrReplaceTempView("payment")
+
+    spark.sql(
+      s"""
+         |SELECT DP.FNUMBER,SUM(OERE.FREALREFUNDAMOUNTFOR) AS REAMOUNT
+         |FROM ${TableName.ODS_ERP_REFUNDBILL} OER
+         |LEFT JOIN ${TableName.ODS_ERP_REFUNDBILLENTRY} OERE ON OER.FID = OERE.FID
+         |LEFT JOIN ${TableName.DIM_PROJECTBASIC} DP ON OERE.FPROJECTNO = DP.FID
+         |left join ${TableName.DIM_ORGANIZATIONS} org on org.forgid=OER.FPAYORGID
+         |WHERE OER.FDOCUMENTSTATUS='C' AND org.fname ='DP咸亨国际科技股份有限公司'
+         |GROUP BY DP.FNUMBER
+         |""".stripMargin).createOrReplaceTempView("refund")
+
+    //最终汇总
+
+    val result = spark.sql(
+      s"""
+         |select
+         |  A.c_order_time,
+         |  A.c_order_id,
+         |  A.c_platform_price,
+         |  A.c_title,
+         |  A.c_receiving_province,
+         |  B.fbillno c_project_no,
+         |  B.FPRICETAXAMOUNT c_fpricetax_amount,
+         |  B.FAMOUNT c_FAMOUNT,
+         |  B.SALENAME c_SALENAME,
+         |  C.SALETAXAMOUNT c_saletaxamount,
+         |  C.SALEAMOUNT c_saleamount,
+         |  C.FCOSTAMTSUM c_fcostamtsum,
+         |  D.SALETAXAMOUNT c_SALETAXAMOUNT_big,
+         |  E.REAMOUNT c_payment_AMOUNT,
+         |  F.REAMOUNT c_refund_amount
+         |from
+         |  pforder A
+         |full join bigproject B on A.c_order_id = B.FCUSTOMERORDERID
+         |left join smallrece C on B.fbillno = C.PRONO
+         |left join bigrece D on B.fbillno = D.PRONO
+         |left join payment E on B.fbillno = E.FNUMBER
+         |left join refund F on B.fbillno = F.FNUMBER
+         |""".stripMargin)
+
+    print("num=========" + result.count())
+    result.coalesce(1).write.csv("/data/file/guanwang")
+
+
+  }
+
+
+  def addressget(infoadd: String) = {
+    AddressAnalysis.provincesMatch(infoadd)
+  }
+}
--- src/main/scala/com/xhgj/bigdata/textProject/WordCount.scala
+++ src/main/scala/com/xhgj/bigdata/textProject/WordCount.scala
@@ -5,45 +5,32 @@ import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 import java.sql.Types.{VARCHAR,INTEGER,DATE}
 import java.util.Properties
+
+/**
+ * double bow
+ *
+ */
 object WordCount {
   def main(args: Array[String]): Unit = {
     //配置不要把mysql字段注释清空
-    val spark = SparkSession
-      .builder()
-      .config("spark.sql.sources.partitionOverwriteMode", "dynamic")
-      .config("spark.sql.sources.jdbc.connectionProperties", "rewriteBatchedStatements=false")
-      .appName("Spark task job Test.scala")
-      .enableHiveSupport()
-      .getOrCreate()
-    import spark.implicits._
-    val df: DataFrame = Seq(
-      ("Tom", "15", "2023-06-12"),
-      ("Jerry", "16", "2023-06-12")
-    ).toDF("projectname", "dept", "createtime")
+    val wordPerMessage = 6
+    var i = 0
+    while (i < 1) {
+      /*
+      1.the (1 to 1) is meaning that only have one circulation.
+      */
+      (1 to 1).foreach { messageNum => {
+        //[There's only three cycle]
+        val str: Seq[Int] = (1 to wordPerMessage).map(x => scala.util.Random.nextInt(32)+1 )
+        val str2=(scala.util.Random.nextInt(15)+1).toString
 
-    // 定义 MySQL 的连接信息
-    val conf = Config.load("config.properties")
-    val url = "jdbc:mysql://172.16.104.238:3306/kettle_test?useInformationSchema=true"
-    val user = conf.getProperty("database.user")
-    val password = conf.getProperty("database.password")
-    val table = "tt4"
-
-    // 定义 JDBC 的相关配置信息
-    val props = new Properties()
-    props.setProperty("user", user)
-    props.setProperty("password", password)
-    props.setProperty("driver", "com.mysql.jdbc.Driver")
-    // 写入 MySQL 数据库DATETIME
-    df.write
-      .mode("overwrite")
-      .option("createTableColumnTypes", "projectname varchar(255) NOT NULL COMMENT '项目名称',dept varchar(128) NOT NULL COMMENT '部门',createtime timestamp NOT NULL COMMENT '创建时间'")  // 明确指定 MySQL 数据库中字段的数据类型
-      .option("batchsize", "10000")
-      .option("truncate", "false")
-      .option("jdbcType", s"projectname=${VARCHAR}, dept =${VARCHAR},createtime=${DATE}")   // 显式指定 SparkSQL 中的数据类型和 MySQL 中的映射关系
-      .jdbc(url, table, props)
-
-    //关闭SparkSession
-    spark.stop()
+        val str3 = str.appended(str2)
+        val str1 = str.mkString(" ") //separate str1 with space
+        println(str3)
+      }
+      }
+      i = i + 1
+    }
   }
 
 }
